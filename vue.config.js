4a209b342c1aeaf12434addf2cad2c683f78d4c9
bojun1995 <bojun1995@foxmail.com>
2020-11-20 23:16:32 +0800

    '【修改】去掉了sass-resources-loader，暂时使用sass-loader'

--- vue.config.js
+++ vue.config.js
@@ -7,21 +7,12 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "./src/styles/base.scss";`,
-        prependData: `@import "./src/styles/common.scss";`,
-        prependData: `@import "./src/styles/variable.scss";`,
+        prependData: `
+          @import "@/styles/base.scss";
+          @import "@/styles/common.scss";
+          @import "@/styles/variable.scss";
+        `,
       },
     },
   },
-  chainWebpack: config => {
-    const oneOfsMap = config.module.rule('scss').oneOfs.store
-    oneOfsMap.forEach(item => {
-        item
-        .use('sass-resources-loader')
-        .loader('sass-resources-loader')
-        .options({
-            resources: [resolvePath('src/styles/common.scss')],
-        })
-    })
-  }
 }
